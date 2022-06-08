3922824729241ccc22f3c0bc0aba43649d0cb90d
VallRax <101760319+VallRax@users.noreply.github.com>
2022-06-08 14:40:35 -0400

    Aun falta terminar carrito carrito.js en proceso

---
+++ static/carrito.js
@@ -0,0 +1,20 @@
+const addToShoppingCarButtons = document.querySelectorAll('.agregar')
+addToShoppingCarButtons.forEach(addToCartButton => {
+    addToCartButton.addEventListener('click', addToCartClicked);
+});
+
+const shoppingCartCardContainer = document.querySelector('.shoppingCartCardContainer');
+
+function addToCartClicked(event) {
+      const button = event.target;
+    const   card1 =  button.closest('.card');
+    
+
+
+    const cardTitle = card1.querySelector('.card-text').textContent;
+    const cardPrice = card1.querySelector('.text-muted').textContent;
+    addItemToShoppingCart(cardTitle, cardPrice);
+
+//}
+//function addItemToShoppingCart(cardTitle,cardPrice){
+//}
