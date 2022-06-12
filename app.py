83c554ac7a29ae830fb91ac157021be4bc175f8a
VallRax <thelastjedi536@gmail.com>
2022-06-12 02:58:50 +0000

    Agregue html de ADMIN

--- app.py
+++ app.py
@@ -37,11 +37,57 @@ def carrito():
 @app.route('/registrar', methods=['POST'])
 def registro():
     data = request.values
-    #FIXME : cambiar al arreglar el formulario
-    #usuario = Usuario(data['nombre'], data['apellido'], data['email'], data['password'])
+    user = Usuario()
+    user.primer_nombre = request.json.get('primer_nombre')
+    user.segundo_nombre = request.json.get('segundo_nombre')
+    user.apellido_paterno = request.json.get('apellido_paterno')
+    user.apellido_materno = request.json.get('apellido_materno')
+    user.direccion = request.json.get('direccion')
+
     #usuario.save()
     print(data.get('nombre'))
     return jsonify({'status': 'ok'})
+
+
+# Ruta para consultar todos los Usuarios
+@app.route('/usuarios', methods=['GET'])
+def getUsuarios():
+    user = Usuario.query.all()
+    user = list(map(lambda x: x.serialize(), user))
+    return jsonify(user),200
+
+
+# Borrar usuario
+@app.route('/usuarios/<id>', methods=['DELETE'])
+def deleteUsuario(id):
+    user = Usuario.query.get(id)
+    Usuario.delete(user)
+    return jsonify(user.serialize()),200
+
+
+# Modificar Usuario
+@app.route('/usuarios/<id>', methods=['PUT'])
+def updateUsuario(id):
+    user = Usuario.query.get(id)
+
+    user.primer_nombre = request.json.get('primer_nombre')
+    user.segundo_nombre = request.json.get('segundo_nombre')
+    user.apellido_paterno = request.json.get('apellido_paterno')
+    user.apellido_materno = request.json.get('apellido_materno')
+    user.direccion = request.json.get('direccion')
+
+    Usuario.save(user)
+
+    return jsonify(user.serialize()),200
+
+
+
+
+
+
+
+
+
 
 @app.route('/login', methods=['POST'])
 def login():
