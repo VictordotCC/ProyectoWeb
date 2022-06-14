f355a9392451c0262dbef3572186629c9d92564c
VictordotCC <victor.celis.c88@gmail.com>
2022-06-14 18:09:53 -0400

    Solo imagen

--- static/scripts/script3.js
+++ static/scripts/script3.js
@@ -30,10 +30,10 @@ $( document ).ready(function(){
     var categoria = $("#inputCategoria").val();
     var precio = $("#inputPrecio").val();
     var stock = $("#inputStock").val();
-    var imagen = $("#inputImagen").val();
+    var imagen = $("#formFile").get(0).files.length;
     var formdata = new FormData($("#formulario")[0]);
 
-    if (nombre == "" || descripcion == "" || categoria == "" || precio == "" || stock == "" || imagen == "") {
+    if (nombre == "" || descripcion == "" || categoria == "" || precio == "" || stock == "" || imagen == 0) {
       alert("Todos los campos son obligatorios");
     }
     else {
@@ -51,4 +51,18 @@ $( document ).ready(function(){
       });
     } 
   });
+
+  $("#formFile").change(function(){
+    if  ($("#formFile").get(0).files.length > 0) {
+      var file = this.files[0];
+      var fileType = file["type"];
+      if (!fileType.includes("image")) {
+        alert("El archivo debe ser una imagen");
+        $("#formFile").val(null)
+      }
+    }
+  });
+
+
+  //Actualizar Stock
 });
